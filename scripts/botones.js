e6bcc9bccbfb84d9fdb4e62e67eff5cef81fc9b0
osunadan <osuna_dan@outlook.com>
2022-07-01 01:53:16 -0500

    Fallbacks Nuestro trabajo

--- scripts/botones.js
+++ scripts/botones.js
@@ -1,13 +1,66 @@
-var videoPlayer = document.getElementById("videoPlayer");
-var myVideo = document.getElementById("myVideo");
+var videoPlayer = document.getElementById("videoPlayer1");
+var videoPlayer2 = document.getElementById("videoPlayer2");
+var videoPlayer3 = document.getElementById("videoPlayer3");
+var videoPlayer4 = document.getElementById("videoPlayer4");
+var videoPlayer5 = document.getElementById("videoPlayer5");
+var videoPlayer6 = document.getElementById("videoPlayer6");
 
 function stopVideo() {
   videoPlayer.style.display = "none";
+  vid.play();
+}
+
+function stopVideo2() {
+  videoPlayer2.style.display = "none";
   vid.play(pauses);
 }
 
-function playVideo(file) {
-  myVideo.src = file;
+function stopVideo3() {
+  videoPlayer3.style.display = "none";
+  vid.play(pauses);
+}
+
+function stopVideo4() {
+  videoPlayer4.style.display = "none";
+  vid.play(pauses);
+}
+
+function stopVideo5() {
+  videoPlayer5.style.display = "none";
+  vid.play(pauses);
+}
+
+function stopVideo6() {
+  videoPlayer6.style.display = "none";
+  vid.play(pauses);
+}
+
+function playVideo1() {
   videoPlayer.style.display = "inline-block";
+  vid.volume = 0.0;
+}
+
+function playVideo2() {
+  videoPlayer2.style.display = "inline-block";
+  vid.volume = 0.0;
+}
+
+function playVideo3() {
+  videoPlayer3.style.display = "inline-block";
+  vid.volume = 0.0;
+}
+
+function playVideo4() {
+  videoPlayer4.style.display = "inline-block";
+  vid.volume = 0.0;
+}
+
+function playVideo5() {
+  videoPlayer5.style.display = "inline-block";
+  vid.volume = 0.0;
+}
+
+function playVideo6() {
+  videoPlayer6.style.display = "inline-block";
   vid.volume = 0.0;
 }
